bd5772d0d08f5ac360a63e9cf5f1c948c5b304e4
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-06-12 02:10:26 +0600

    latest-part

--- src/componants/Home/Home.js
+++ src/componants/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Bike from '../MyImage/pexels-cottonbro-5195634.jpg';
+import Bike1 from '../MyImage/pexels-arthouse-studio-6068905.jpg';
 import './Home.css';
 import { FcSearch } from 'react-icons/fc';
 // 
@@ -20,12 +21,42 @@ const Home = () => {
 
                    
                 </h1>
-                <h2 className='text-center mt-5'><input className='field' type="text" name="" id="" placeholder='serarch for bike'    /><button className='btn btn-outline-primary mb-2'>Search <FcSearch></FcSearch></button></h2>
+                <h2 className='text-center mt-5'><input className='field' type="text" name="" id="" placeholder='serarch for bike'    /><button className='btn btn-success mb-2'>Search <FcSearch></FcSearch></button></h2>
                 
                 </div>
                
                     
 
+            </div>
+            <div className='setion1'>
+                <div className='text-part'>
+                    <p> wooden rims, a leading-link front suspension fork,
+                         a Garford spring saddle, a Duck Brake Company front roller brake,
+                          and a 1902-patent Atherton rear coaster brake.[2][5][7] A leather belt-drive with a
+                           spring-loaded idler pulley directly connected the engine output shaft to the rear wheel.[5] Using 
+                           a standard steel bicycle frame, the California weighed approximately 70–80 pounds (32–36 kg) without rider, and was
+                           
+                     capable of approximately 25 mph (40 km/h) using the 30-octane gasoline of the day, with a range of 75 to
+                      100 miles (121 to 161 km).[5][8] Throttle control was not yet perfected, and engine revolutions were mainly 
+                      controlled by means of a spark timing mechanism.[5] The wick-type carburetor was crude, consisting of a metal
+                       box with internal baffles stuffed with cotton
+                         batting.[9] With no float chamber, the rider had to open the gasoline tap periodically to admit fuel into the carburetor.
+                     capable of approximately 25 mph (40 km/h) using the 30-octane gasoline of the day, with a range of 75 to
+                      100 miles (121 to 161 km).[5][8] Throttle control was not yet perfected, and engine revolutions were mainly 
+                      controlled by means of a spark timing mechanism.[5] The wick-type carburetor was crude, consisting of a metal
+                       box with internal baffles stuffed with cotton
+                         batting.[9] With no float chamber, the rider had to open the gasoline tap periodically to admit fuel into the carburetor.
+                     capable of approximately 25 mph (40 km/h) using the 30-octane gasoline of the day, with a range of 75 to
+                      100 miles (121 to 161 km).[5][8] Throttle control was not yet perfected, and engine revolutions were mainly 
+                      controlled by means of a spark timing mechanism.[5] The wick-type carburetor was crude, consisting of a metal
+                       box with internal baffles stuffed with cotton
+                         batting.[9] With no float chamber, the rider had to open the gasoline tap periodically to admit fuel into the carburetor.</p>
+
+                </div>
+                <div className='imgg-part'>
+                    <img  className='img-part' src={Bike1} alt="" />
+
+                </div>
             </div>
             
         </div>
